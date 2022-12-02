32a2768ef0e574f8219a9c18cf29952b81afc1f7
Marc Dannemann <mdannemann@hs-harz.de>
2022-12-02 10:20:53 +0100

    Upgrade to .net 6.0 and updated copyright

--- src/SharpVk.Generator/Emission/FileBuilderFactory.cs
+++ src/SharpVk.Generator/Emission/FileBuilderFactory.cs
@@ -28,7 +28,7 @@ namespace SharpVk.Generator.Emission
             {
                 builder.EmitComment($@"The MIT License (MIT)
 
-Copyright (c) Andrew Armstrong/FacticiusVir {DateTime.UtcNow.Year}
+Copyright (c) Marc Dannemann/Data1992 {DateTime.UtcNow.Year}
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the ""Software""), to deal
